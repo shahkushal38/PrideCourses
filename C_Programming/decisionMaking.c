2b943e17be3f3a79143540353b7f622b2495c7ae
Kushal Shah <60709835+shahkushal38@users.noreply.github.com>
2023-02-24 14:00:23 +0000

    added frameset.html

--- C_Programming/decisionMaking.c
+++ C_Programming/decisionMaking.c
@@ -54,3 +54,54 @@ void main(){
     }
     getch();
 }
+
+--Question 
+Give marks of 4 subjects English, Maths, Science, History (out of 100)
+Calculate average (float)
+
+>90 = Grade A 
+>80 and <=89 = grade B 
+>70 and <=79 = Grade C
+>60 and <=69 = Grade D 
+<60 = Grade E 
+
+switch(a){
+
+    case 'a':
+        printf("/n The month is ");
+        break;
+    case 'b':
+        printf()
+        break;
+    case 'c':
+        break;
+    case 'd':
+        break;
+    default:
+        //code
+        break;
+}
+
+
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+    int month;
+    clrscr();
+    printf("\nEnter the month number -  ");
+    scanf("%d", &month);
+
+    switch(month){
+        case 1:
+            printf("\n Month is January");
+            break;
+        case 2:
+            printf("\n Month is February");
+            break;
+        default:
+            printf("Invalid");
+
+    }
+    getch();
+}
