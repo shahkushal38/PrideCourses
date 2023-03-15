522bed4438cac00a0d6e400a482dd05c089779c2
Kushal Shah <60709835+shahkushal38@users.noreply.github.com>
2023-03-15 14:25:08 +0000

    java new

---
+++ Java_Program/intro.java
@@ -0,0 +1,26 @@
+-Java Introduction
+
+C, C++, Python 
+Core Java - Purely Object Oriented Programming Language 
+
+Java - Security Features
+course structure - 
+static this
+Data Types, For loops, functions, OOPs - Inheritance, Abstraction, Polymorphism, Encapsulation 
+
+advance Java - 
+J2E - Spring Framework , Hibernate, Hadoop
+J2EE - Netbeans
+
+JDBC - SQL Connect 
+Servers, Servlets, -
+
+
+Java Installation - 
+jdk - Java Development Kit (modules- Maths, Lang, io,)
+jvm - Java Virtual Machine - Run and Compile code 
+
+Compile code .java
+Compile and Run
+
+Java jdk - 8 - Collection Frameworks
