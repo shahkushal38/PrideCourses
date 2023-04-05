c2c8000b746848e90653d32ac6328148597f6a79
Kushal Shah <60709835+shahkushal38@users.noreply.github.com>
2023-04-05 11:57:21 +0000

    functions.c

--- C_Programming/functions.c
+++ C_Programming/functions.c
@@ -1,5 +1,24 @@
 Functions -
 
+->  simple block of code
+
+no return no parameters/arguments
+no return and with parameters
+return and no parameters
+return and parameters
+
+
+void = null, no return value
+
+specifier
+void/int/char/float function_name(){
+
+    i
+}
+no return no arguments
+
+
+
 #include<stdio.h>
 #include<conio.h>
 
@@ -20,9 +39,17 @@ void prime(){
     printf("\n Prime function called");
 }
 
+// return and arguments
+int addition(int a, int b){
+    int ans = a+b;
+    return ans;
+
+}
 void main(){
     clrscr();
     func();
     prime();
+    int a = 10;
+    int sum = addition(a, 20);
     getch();
 }
